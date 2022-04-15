44d573871ae7d750bd474353807650f4e381d8b1
chris097 <christian@esettlementgroup.com>
2022-04-15 23:54:00 +0100

    request data from server

--- src/pages/Home/HomeComp.js
+++ src/pages/Home/HomeComp.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFetchApi } from "../../services/fetchApi";
 
 
 const HomeComp = () => {
+
+    const [ breedName, setBreedName ] = useState("affenpinscher");
+
     const { data } = useFetchApi("breeds/list");
-    const {data:isData} = useFetchApi("breed/australian/images")
+    const {data:isData} = useFetchApi(`breed/${breedName}/images`);
     const breed = data?.message;
-    console.log(isData?.message)
+
     return(
         <div className="max-w-7xl md:mx-auto mx-10">
             <div className="md:flex md:justify-between space-y-6 items-center flex-wrap mt-24 md:mt-52">
                 <div className="md:flex space-y-6 md:space-y-0 md:space-x-6">
-                    <select className="border border-black w-full md:w-40 py-3 px-1 outline-none">
-                        <option>Breeds</option>
+                    <select onChange={(e) => setBreedName(e.target.value)} className="border border-black w-full md:w-40 py-3 px-1 outline-none">
                         {breed?.map(name => (
                             <option>{name}</option>
                         ))}
@@ -33,7 +35,7 @@ const HomeComp = () => {
                 <div className="mt-6 flex space-y-7 md:space-y-0 md:space-x-7 flex-wrap">
                     <div className="w-350px md:h-456px space-y-6">
                         {isData?.message?.map(msg => (
-                            <img className="h-400 md:h-456px object-fit bg-cover bg-no-repeat" src={msg} alt="dog_image" />
+                            <img className="h-400px md:w-350px w-full md:h-456px object-fit bg-center" src={msg} alt="dog_image" />
                         ))}
                     </div>
                 </div>
